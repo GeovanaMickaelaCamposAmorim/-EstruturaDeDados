897cb59f6fc4f46dfdab39bd060990b6f2751778
Geovana Amorim <101568045+GeovanaMickaelaCamposAmorim@users.noreply.github.com>
2022-08-31 11:46:47 -0300

    Fatorial

---
+++ Fatorial.java
@@ -0,0 +1,18 @@
+package Fatorial;
+
+public class Fatorial {
+
+	long total;
+	
+	public long contar ( long n) {
+		if(n == 1) {
+			return 1;
+		}
+		 System.out.println("Pilha-->"+n);
+	
+	total = n*contar(n-1);
+	System.out.println("Desmontagem da pilha -->"+total);
+	 return total;
+}
+}
+ 
---
+++ Principal.java
@@ -0,0 +1,13 @@
+package Fatorial;
+
+public class Principal {
+
+	
+	public static void main(String[] args) {
+		
+		Fatorial r = new Fatorial();
+		
+		System.out.println(r.contar(12));
+		
+	} 
+}
